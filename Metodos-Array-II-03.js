daed96d5f80c1f90a90e184d3c3fa9a25e3607d2
Ismael <ismael.dantas@hotmail.com>
2023-10-26 12:46:12 -0300

    Finaliazando o exercicio 3 das aulas de metodos de array

--- Metodos-Array-II-03.js
+++ Metodos-Array-II-03.js
@@ -1,6 +1,14 @@
-const usuarios = [
-  { nome: 'Ismael', idade: 26 },
-  { nome: 'Claudia', idade: 51 },
-  { nome: 'Teyce', idade: 31 },
-  { nome: 'Fernanda', idade: 23 }
+const carros = [
+  { nomeMarca: 'Chevrolet', modelo: 'Monza', ano: 1989, cor: 'Prata' },
+  { nomeMarca: 'Volkswagen', modelo: 'Gol', ano: 2013, cor: 'Prata' },
+  { nomeMarca: 'Honda', modelo: 'CRV', ano: 2008, cor: 'Preto' },
+  { nomeMarca: 'Volkswagen', modelo: 'Santana', ano: 2000, cor: 'Branco' }
 ]
+
+const buscaMarcas = (marca, arrayCarros) => {
+  return arrayCarros.find(carro => {
+    return carro.nomeMarca === marca
+  })
+}
+
+console.log(buscaMarcas('Honda', carros))
